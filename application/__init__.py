e6589e80023c992fb65b246bc8a8dac9154b4057
Dohee Lee <dojuhee2015@gmail.com>
2023-06-09 02:12:26 +0100

    hide the url

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,15 @@
+import os
 import secrets
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = secrets.token_hex(16)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bdzmzmsh:4uTi9Dh-sMky0_ROhYjzsgWWJMJE-JVT@lucky.db.elephantsql.com/bdzmzmsh'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
 
 db = SQLAlchemy(app)
