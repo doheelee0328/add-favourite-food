7f767b1fc3257610b3b6098f4d5dc52fed4fa0ec
Dohee Lee <dojuhee2015@gmail.com>
2023-06-09 01:36:10 +0100

    make a  few changes

--- application/forms.py
+++ application/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,  SubmitField, IntegerField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, NumberRange
 
 class UserDataForm(FlaskForm):
   item = StringField("Item", validators =[DataRequired()])
-  votes = IntegerField("Votes", validators=[DataRequired()])
+  votes = IntegerField("Votes", validators=[DataRequired(), NumberRange(min=1, max=10)])
   submit = SubmitField("Generate Item")
 
 class UpdateUserDataForm(FlaskForm):
   item = StringField("Item", validators =[DataRequired()])
-  votes = IntegerField("Votes", validators=[DataRequired()])
-  submit = SubmitField("Update Item")
+  votes = IntegerField("Votes", validators=[DataRequired(), NumberRange(min=1, max=10)])
+  submit = SubmitField("Save")
